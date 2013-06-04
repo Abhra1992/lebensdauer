fb9ad6d61da604fab5298e068eb5760fec77d011
Abhra Basak <abhra.1992@live.com>
2013-06-04 14:06:40 +0530

    Lebensdauer Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lebensdauer::Application.config.secret_token = '305bbb52420da4e3162ae3fa62189e45e5aa132bc615b692c29635bd936e0e0f26d5769a68479addda0c51681d5c19b12cf2a1ac8850f429cdd9e5a194bf0f03'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lebensdauer::Application.config.session_store :cookie_store, key: '_lebensdauer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lebensdauer::Application.config.session_store :active_record_store
